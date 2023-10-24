b4f3adf673cfcfd7b20217aa374498448a7e683e
JCav23 <>
2023-10-24 15:03:37 +0000

    added begining of startGame function to script so buttons now update player image

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,22 @@ document.addEventListener("DOMContentLoaded", function(){
     for (let button of buttons) {
         button.addEventListener("click", function() {
             let gameChoice = this.getAttribute("data-type");
-            console.log(gameChoice);
+            startGame(gameChoice)
         });
     }
 });
+
+function startGame(gameChoice) {
+    let playerImage = document.getElementById("player-img");
+    if (gameChoice == "rock") {
+        playerImage.setAttribute("src", "assets/images/rock.png")
+    } else if (gameChoice == "paper"){
+        playerImage.setAttribute("src", "assets/images/paper.png")
+    } else if (gameChoice == "scissors"){
+        playerImage.setAttribute("src", "assets/images/scissors.png")
+    } else if (gameChoice == "lizard"){
+        playerImage.setAttribute("src", "assets/images/lizard.png")
+    } else if (gameChoice == "spock"){
+        playerImage.setAttribute("src", "assets/images/spock.jpg")
+    }
+}
