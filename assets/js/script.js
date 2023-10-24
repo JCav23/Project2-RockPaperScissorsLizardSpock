d2944629ef22ecc9c068157f693c3f3869e8502a
JCav23 <>
2023-10-24 14:27:54 +0000

    added 'DOMContentLoaded' function with console.log test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
+// used for testing purposes
 console.log("Bazinga!");
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            let gameChoice = this.getAttribute("data-type");
+            console.log(gameChoice);
+        });
+    }
+});
