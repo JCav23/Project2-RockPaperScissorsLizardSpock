ef3fc665787c5ca3d69d7babb4d30d669b48949f
Jack Cavanagh <jack.m.cavanagh@hotmail.co.uk>
2023-10-25 12:24:47 +0000

    added base structure to evaluateWinner function
    base structure evalutates both results and updates in case of tie

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,14 +16,19 @@ function startGame(gameChoice) {
     let playerImage = document.getElementById("player-img");
     if (gameChoice == "rock") {
         playerImage.setAttribute("src", "assets/images/rock.png")
+        playerImage.setAttribute("alt", "rock")
     } else if (gameChoice == "paper"){
         playerImage.setAttribute("src", "assets/images/paper.png")
+        playerImage.setAttribute("alt", "paper")
     } else if (gameChoice == "scissors"){
         playerImage.setAttribute("src", "assets/images/scissors.png")
+        playerImage.setAttribute("alt", "scissors")
     } else if (gameChoice == "lizard"){
         playerImage.setAttribute("src", "assets/images/lizard.png")
+        playerImage.setAttribute("alt", "lizard")
     } else if (gameChoice == "spock"){
         playerImage.setAttribute("src", "assets/images/spock.png")
+        playerImage.setAttribute("alt", "spock")
     }
 }
 
@@ -33,4 +38,19 @@ function compChoice() {
     let finalChoice = choices[randomChoice];
     let compImage = document.getElementById('computer-img');
     compImage.setAttribute("src", `assets/images/${finalChoice}.png`)
+    compImage.setAttribute("alt", `${finalChoice}`)
+    evaluateWinner();
 }
+
+function evaluateWinner() {
+    let results = ["You Win!", "Bazinga! You Lose.", "It's a Tie..."];
+    let playerImage = document.getElementById('player-img');
+    let compImage = document.getElementById('computer-img');
+    let playerResult = playerImage.getAttribute('alt');
+    let compResult = compImage.getAttribute('alt');
+    let output = document.getElementById('results');
+    if (playerResult === compResult) {
+        output.innerHTML = `You both chose ${playerResult}, ${results[2]}`;
+        console.log(playerResult, results[2]);
+    }
+};
